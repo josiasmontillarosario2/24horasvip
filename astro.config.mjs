bfeedf46c9dc8a787d5c7badacf03caa7a289dbf
Josias Montilla <josiasmontillarosario2@gmail.com>
2025-05-03 14:18:16 -0400

    last checked

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,5 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    // Opciones específicas para el adaptador de Vercel
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920],
-      domains: ['www.telemundo.com'], // Dominios permitidos para imágenes
-    },
-    // Opcional: configuración específica para el edge runtime
-    edgeMiddleware: true,
-    // Extensiones de archivo para incluir en el empaquetado edge
-    includeFiles: ['**/*.{jpg,png,svg,woff,woff2}'],
-  }),
+  adapter: vercel(),
 });
